a804a6e7cec4626213f1686784b58c683dd5e519
Samantha Chuang <ssc538@nyu.edu>
2018-10-15 06:56:35 -0400

    2018.10.15 Player movement Walls Block that can be pushed around

---
+++ Grammy Sammy's Grid Game/Assets/Player/NewPlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerController : MonoBehaviour
+{
+
+	private bool canMove = true;
+	private bool moving = false;
+	private int speed = 5;
+	private int buttonCooldown = 0;
+	private Vector3 pos;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		buttonCooldown--;
+		
+		if (canMove)
+		{
+			pos = transform.position;
+			move(); 
+		}
+
+		if (moving)
+		{
+			if (transform.position == pos)
+			{
+				moving = false;
+				canMove = true;
+
+				move();
+			}
+
+			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+		}
+	}
+
+	private void move()
+	{
+		if (buttonCooldown <= 0)
+		{
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				canMove = false;
+				moving = true;
+				pos += Vector3.up;
+			}
+			else if (Input.GetKey(KeyCode.DownArrow))
+			{
+				canMove = false;
+				moving = true;
+				pos += Vector3.down;
+			}
+			else if (Input.GetKey(KeyCode.RightArrow))
+			{
+				canMove = false;
+				moving = true;
+				pos += Vector3.right;
+			}
+			else if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				canMove = false;
+				moving = true;
+				pos += Vector3.left;
+			}
+		}
+	}
+}
