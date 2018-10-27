e49a265500e7bd5f50e59978812e245b2b284b18
Samantha Chuang <ssc538@nyu.edu>
2018-10-27 15:56:49 -0400

    2018.10.27 update: - Player not getting stuck to walls anymore! Yeh! :] - Working on grid movement for the box

--- Grammy Sammy's Grid Game/Assets/Scripts/Box.cs
+++ Grammy Sammy's Grid Game/Assets/Scripts/Box.cs
@@ -1,16 +1,107 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Box : MonoBehaviour {
+public class Box : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	private bool canMove;
+	private bool isMoving;
+	private int speed = 5;
+	private Vector3 towardsPos;
+	private Vector3 towardsPosCheck;
+
+	private GameObject[] walls;
+	private GameObject[] boxes;
+
+	void Start()
+	{
+		//walls = 8;
+		canMove = true;
+		isMoving = false;
+		speed = 5;
+		walls = GameObject.FindGameObjectsWithTag("Wall");
+		boxes = GameObject.FindGameObjectsWithTag("Box");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+//		if (canMove && !isMoving)
+//		{
+//			towardsPos = transform.position;
+//			towardsPosCheck = transform.position;
+//			MoveBox();
+//		}
+//
+//		if (isMoving)
+//		{
+//			//if the player has reached the next grid spot, they can move again
+//			if (transform.position == towardsPos)
+//			{
+//				isMoving = false;
+//				canMove = true;
+//			}
+//
+//			transform.position = Vector3.MoveTowards(transform.position, towardsPos, Time.deltaTime * speed);
+//		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void MoveBox()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			canMove = false;
+			isMoving = true;
+			towardsPos += Vector3.up;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			canMove = false;
+			isMoving = true;
+			towardsPos += Vector3.down;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			canMove = false;
+			isMoving = true;
+			towardsPos += Vector3.right;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			canMove = false;
+			isMoving = true;
+			towardsPos += Vector3.left;
+		}
+	}
+
+	public bool BoxBlocked()
+	{
+		foreach (GameObject box in boxes)
+		{
+			if (Input.GetKey(KeyCode.UpArrow) && this.transform.position == box.transform.position)
+			{
+				return true;
+			}
+			else if (Input.GetKey(KeyCode.DownArrow) && this.transform.position == box.transform.position)
+			{
+				return true;
+			}
+			else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position == box.transform.position)
+			{
+				return true;
+			}
+			else if (Input.GetKey(KeyCode.LeftArrow) &&this.transform.position == box.transform.position)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		return false; 
 	}
 }
--- Grammy Sammy's Grid Game/Assets/Scripts/NewPlayerController.cs
+++ Grammy Sammy's Grid Game/Assets/Scripts/NewPlayerController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NewPlayerController : MonoBehaviour
 {
 
-	private enum direction
+	public enum direction
 	{
 		RIGHT,
 		LEFT,
@@ -17,29 +18,36 @@ public class NewPlayerController : MonoBehaviour
 	private bool isMoving;
 	private int speed = 5;
 	private Vector3 towardsPos;
-	private direction orientation; 
+	private Vector3 towardsPosCheck;
+	private direction orientation;
+
+	//private int walls;
 
 	//check to see if the player is next to a wall, if it is, then do NOT run moveTowards 
 	private bool isNear;
 	private GameObject[] walls;
+	private GameObject[] boxes;
 
 
 	// Use this for initialization
 	void Start()
 	{
+		//walls = 8;
 		canMove = true;
 		isMoving = false;
-		speed = 5; 
+		speed = 5;
 		walls = GameObject.FindGameObjectsWithTag("Wall");
+		boxes = GameObject.FindGameObjectsWithTag("Box");
 		orientation = direction.RIGHT;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (canMove)
+		if (canMove && !isMoving)
 		{
 			towardsPos = transform.position;
+			towardsPosCheck = transform.position;
 			move();
 		}
 
@@ -50,8 +58,7 @@ public class NewPlayerController : MonoBehaviour
 			{
 				isMoving = false;
 				canMove = true;
-
-				move();
+				//move();
 			}
 
 			transform.position = Vector3.MoveTowards(transform.position, towardsPos, Time.deltaTime * speed);
@@ -60,28 +67,28 @@ public class NewPlayerController : MonoBehaviour
 
 	private void move()
 	{
-		if (Input.GetKey(KeyCode.UpArrow) && !Blocked(towardsPos += Vector3.up))
+		if (Input.GetKey(KeyCode.UpArrow) && !Blocked(towardsPosCheck += Vector3.up))
 		{
 			orientation = direction.UP;
 			canMove = false;
 			isMoving = true;
 			towardsPos += Vector3.up;
 		}
-		else if (Input.GetKey(KeyCode.DownArrow) && !Blocked(towardsPos += Vector3.down))
+		else if (Input.GetKey(KeyCode.DownArrow) && !Blocked(towardsPosCheck += Vector3.down))
 		{
 			orientation = direction.DOWN;
 			canMove = false;
 			isMoving = true;
 			towardsPos += Vector3.down;
 		}
-		else if (Input.GetKey(KeyCode.RightArrow) && !Blocked(towardsPos += Vector3.right))
+		else if (Input.GetKey(KeyCode.RightArrow) && !Blocked(towardsPosCheck += Vector3.right))
 		{
 			orientation = direction.RIGHT;
 			canMove = false;
 			isMoving = true;
 			towardsPos += Vector3.right;
 		}
-		else if (Input.GetKey(KeyCode.LeftArrow) && !Blocked(towardsPos += Vector3.left)) 
+		else if (Input.GetKey(KeyCode.LeftArrow) && !Blocked(towardsPosCheck += Vector3.left))
 		{
 			orientation = direction.LEFT;
 			canMove = false;
@@ -90,19 +97,58 @@ public class NewPlayerController : MonoBehaviour
 		}
 	}
 
-	bool Blocked(Vector3 towardsPos)
+	bool Blocked(Vector3 towardsPosCheck)
 	{
 		foreach (GameObject wall in walls)
 		{
-			if (wall.transform.position == towardsPos)
+			if (wall.transform.position == towardsPosCheck)
 			{
-				Debug.Log("There is a wall so I cannot move");
+				Debug.Log("there is a wall so therefore i cannot move");
 				return true;
 			}
 		}
 
+		//return false;
+
+//		foreach (GameObject box in boxes)
+//		{
+//			if (box.GetComponent<Box>().BoxBlocked())
+//			{
+//				Debug.Log("there is a box behind the box so I cannot push it");
+//				return true;
+//			}
+//		}
 		return false;
 	}
+
+	private void OnTriggerEnter2D(Collider2D otherObj)
+	{
+		if (otherObj.gameObject.CompareTag("Box"))
+		{
+			otherObj.GetComponent<Box>().MoveBox();
+		}
+	}
+}
+
+//	bool Blocked(Vector3 towardsPosCheck)
+//	{
+//		Vector2 positionCheck = new Vector2(towardsPosCheck.x, towardsPosCheck.y);
+//
+//		Collider2D overlap = Physics2D.OverlapPoint(positionCheck, walls, 0, 0);
+//		Debug.Log(overlap);
+//		
+//		if (overlap != null)
+//		{
+//			Debug.Log(overlap);
+//			return true; 
+//		}
+//		
+//		return false;
+//	}
+//}
+
+
+
 
 //	void checkInFront()
 //	{
@@ -121,15 +167,13 @@ public class NewPlayerController : MonoBehaviour
 //		
 //	}
 
-	void OnTriggerEnter2D(Collider2D otherObj)
-	{
-		if (otherObj.CompareTag("Wall"))
-		{
-			Debug.Log("hit a wall, can't move!");
-			canMove = false; 
-		}
-	}
-}
+//		if (otherObj.CompareTag("Wall"))
+//		{
+//			Debug.Log("hit a wall, can't move!");
+//			canMove = false; 
+//		}
+//	}
+//}
 
 //		Vector3 dir = (this.transform.position - transform.position).normalized;
 //		float dot = Vector3.Dot(dir, transform.forward);
